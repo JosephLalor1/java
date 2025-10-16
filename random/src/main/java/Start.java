12ab686ceecd9b18980a25e26011cb01018ef20b
Joseph Lalor <joseph.lalor@gmail.com>
2025-10-16 22:04:10 +0100

    x

--- random/src/main/java/Start.java
+++ random/src/main/java/Start.java
@@ -1,11 +1,13 @@
 
 
 
+import java.net.MalformedURLException;
+
 import javax.swing.JFrame;
 
 
 public class Start {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws MalformedURLException
         {
             StartFrame.startFrame();
             Operations.Connect();
--- random/src/main/java/StartFrame.java
+++ random/src/main/java/StartFrame.java
@@ -1,18 +1,24 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 public class StartFrame 
     {
-        public static void startFrame()
+        public static void startFrame() throws MalformedURLException
             {
                 JFrame starter = new JFrame("Welcome!");
+                URL url = new URL("images/icons/Welcome!.gif");
                 starter.setSize(300, 300);
                 starter.setVisible(true);
-                starter.add(new JLabel(new ImageIcon("images/icons/Welcome!.gif")));
+                starter.add(new JLabel(new ImageIcon(url)));
 
                 Timer timer = new Timer(5000, e -> {
                     starter.dispose();
-                });                
+                });
+                timer.setRepeats(false);
+                timer.start();              
             }
     }
